75034fdf2e2fd7755d7aa552cf48b0b633d46f4a
gumara <gumaraa@gmail.com>
2017-04-25 09:46:00 +0700

    change score form step 1 to step 0.1

--- score.php
+++ score.php
@@ -79,7 +79,7 @@ if ($result!=null) {
     <input type="hidden" name="position_id<?=$i;?>" value="<?=$row_pos['position_id'];?>">
     <div class="form-group">
       คะแนน (ไม่เกิน 65 คะแนน)
-      <input type="number" class="form-control score_form" id="score_<?=$i;?>" placeholder="คะแนนของตำแหน่งที่เลือก <?=$i;?>" name="score_<?=$i;?>" value="<?=$row_pos['score'];?>" min="0" max="65.0" step="1" required>
+      <input type="number" class="form-control score_form" id="score_<?=$i;?>" placeholder="คะแนนของตำแหน่งที่เลือก <?=$i;?>" name="score_<?=$i;?>" value="<?=$row_pos['score'];?>" min="0" max="65.0" step="0.1" required>
     </div>
   </div>
   <!-- position score -->
